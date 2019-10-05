1ed885f49d5efeea0deb23fc04f5440abda1fb8a
yeo11200 <yeo11200@hanmail.net>
2019-10-05 10:43:29 +0900

    Fix not url root to servlet-context.xml

--- src/main/java/com/smallanimals/notice/vo/NoticeVO.java
+++ src/main/java/com/smallanimals/notice/vo/NoticeVO.java
@@ -6,12 +6,58 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
-@Getter
-@Setter
 public class NoticeVO {
 	public int no, emp;
 	public String title, writer, content;
 	public Date startDate, endDate, writeDate;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getEmp() {
+		return emp;
+	}
+	public void setEmp(int emp) {
+		this.emp = emp;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public Date getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(Date writeDate) {
+		this.writeDate = writeDate;
+	}
 
+	
 }
