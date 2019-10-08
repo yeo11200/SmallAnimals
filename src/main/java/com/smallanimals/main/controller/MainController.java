7b20390846fa4304fc5a01f069d2f835cbfb32f2
You-jin <uj2671@gmail.com>
2019-10-08 21:55:29 +0900

    Add register

--- src/main/java/com/smallanimals/main/controller/MainController.java
+++ src/main/java/com/smallanimals/main/controller/MainController.java
@@ -1,12 +1,20 @@
 package com.smallanimals.main.controller;
 
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
+ Logger logger = LoggerFactory.getLogger(getClass());
 	@GetMapping(value="/")
 	public String main() {
 		return "redirect:/main/main";
@@ -16,4 +24,38 @@ public class MainController {
 	public ModelAndView main1() {
 		return new ModelAndView("/main/main");
 	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String register(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		
+		
+		return "register";
+	}
+	@RequestMapping(value = "/naver", method = RequestMethod.GET)
+	public String naver(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		
+		
+		return "naver";
+}
+	@RequestMapping(value = "/sample", method = RequestMethod.GET)
+	public String sample(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		
+		
+		return "sample";
+}
+	@RequestMapping(value = "/jusoPopup", method = RequestMethod.GET)
+	public String jusoPopup(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		
+		
+		return "jusoPopup";
+}
+	
 }
