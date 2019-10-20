2e58cd4e33f95acfb15db1438e9000857a8d1f19
You-jin <uj2671@gmail.com>
2019-10-20 16:01:57 +0900

    register modify

---
+++ src/main/java/com/smallanimals/member/dto/MemberDTO.java
@@ -0,0 +1,107 @@
+package com.smallanimals.member.dto;
+
+import java.util.Date;
+
+public class MemberDTO {
+	private int user_no;
+	private String user_id;
+	private String user_pwd;
+	private String user_name;
+	private String user_nickname;
+	private String user_phone;
+	private String user_addr;
+	private Date user_regdate;
+	private Date user_loginDate;
+	private String user_ckeckparcel;
+	private String user_parcel;
+	private String user_enabled;
+	private String user_type;
+	private String user_role;
+	public int getUser_no() {
+		return user_no;
+	}
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_pwd() {
+		return user_pwd;
+	}
+	public void setUser_pwd(String user_pwd) {
+		this.user_pwd = user_pwd;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_nickname() {
+		return user_nickname;
+	}
+	public void setUser_nickname(String user_nickname) {
+		this.user_nickname = user_nickname;
+	}
+	public String getUser_phone() {
+		return user_phone;
+	}
+	public void setUser_phone(String user_phone) {
+		this.user_phone = user_phone;
+	}
+	public String getUser_addr() {
+		return user_addr;
+	}
+	public void setUser_addr(String user_addr) {
+		this.user_addr = user_addr;
+	}
+	public Date getUser_regdate() {
+		return user_regdate;
+	}
+	public void setUser_regdate(Date user_regdate) {
+		this.user_regdate = user_regdate;
+	}
+	public Date getUser_loginDate() {
+		return user_loginDate;
+	}
+	public void setUser_loginDate(Date user_loginDate) {
+		this.user_loginDate = user_loginDate;
+	}
+	public String getUser_ckeckparcel() {
+		return user_ckeckparcel;
+	}
+	public void setUser_ckeckparcel(String user_ckeckparcel) {
+		this.user_ckeckparcel = user_ckeckparcel;
+	}
+	public String getUser_parcel() {
+		return user_parcel;
+	}
+	public void setUser_parcel(String user_parcel) {
+		this.user_parcel = user_parcel;
+	}
+	public String getUser_enabled() {
+		return user_enabled;
+	}
+	public void setUser_enabled(String user_enabled) {
+		this.user_enabled = user_enabled;
+	}
+	public String getUser_type() {
+		return user_type;
+	}
+	public void setUser_type(String user_type) {
+		this.user_type = user_type;
+	}
+	public String getUser_role() {
+		return user_role;
+	}
+	public void setUser_role(String user_role) {
+		this.user_role = user_role;
+	}
+	
+	
+	
+}
